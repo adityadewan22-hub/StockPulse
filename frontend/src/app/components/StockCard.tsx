c6abce5d80bc82da2dca52acf86db1b4cb2eecb8
Aditya Dewan <adityadewan22@gmail.com>
2025-10-07 15:46:39 +0530

    added basic stockCard to check workflow

---
+++ frontend/src/app/components/StockCard.tsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+const StockCard = ({ symbol }: { symbol: string }) => {
+  const [data, setData] = useState<any>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await axios.get(`http://localhost:5000/${symbol}`);
+      setData(res.data);
+    };
+    fetchData();
+  }, [symbol]);
+
+  return (
+    <div>
+      <h2>
+        {symbol}
+        <pre>
+          {JSON.stringify(
+            data?.["Time Series (5min)"]?.[
+              Object.keys(data?.["Time Series (5min)"] || {})[0]
+            ],
+            null,
+            2
+          )}
+        </pre>
+      </h2>
+    </div>
+  );
+};
