a6d7e5c6a45435dfc3e10a1fd7a71268299f415c
Aditya Dewan <adityadewan22@gmail.com>
2025-10-14 16:05:18 +0530

    bug fixed to disconnect and switch to socket.io if redis not available

--- backend/src/config/cache.js
+++ backend/src/config/cache.js
@@ -1,10 +1,27 @@
-import client from "./redisClient.js";
+import {client} from "./redisClient.js";
+import { redisAvailable } from "./redisClient.js";
 
 export const getCache= async(key)=>{
-    const cached=await client.get(key);
+    if(!redisAvailable){
+        return null;
+    }
+    try{
+        const cached=await client.get(key);
     return cached?JSON.parse(cached):null;
+    }catch(err){
+        console.log("redis get failed");
+        return null;
+    }
+    
 }
 
 export const setCache=async(key,data,ttl=15)=>{
-    await client.set(key,JSON.stringify(data),{EX:ttl});
+    if(!redisAvailable){
+        return null;
+    }
+    try{
+        await client.set(key,JSON.stringify(data),{EX:ttl});
+    }catch(err){
+        console.log("redis set failed")
+    }
 }
--- backend/src/config/redisClient.js
+++ backend/src/config/redisClient.js
@@ -1,22 +1,28 @@
 
-import {createClient} from "redis";
+import { createClient } from "redis";
 
-const client =createClient({
-    url:"redis://localhost:6379",
-});
+let client;
+let redisAvailable = false;
 
-client.on("error",(err)=>console.error("Redis Client error",err));
+try {
+  client = createClient({
+    url: "redis://localhost:6379",
+    socket: {
+      reconnectStrategy: false, 
+    },
+  });
 
-async function connectRedis() {
-    try{
-        await client.connect();
-        console.log("connected to redis");
-    }
-    catch(err){
-        console.log("redis not available");
-    }
+  client.on("error", (err) => {
+    console.log("⚠️ Redis not available:", err.message);
+    redisAvailable = false;
+  });
+
+  await client.connect();
+  redisAvailable = true;
+  console.log("✅ Redis connected");
+} catch (err) {
+  console.log("⚠️ Failed to connect to Redis:", err.message);
+  redisAvailable = false;
 }
 
-connectRedis();
-
-export default client;
+export { client, redisAvailable };
