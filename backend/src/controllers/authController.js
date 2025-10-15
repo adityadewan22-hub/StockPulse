39fba5ae3850207b68727fb672f9a4fb913d3e9e
Aditya Dewan <adityadewan22@gmail.com>
2025-10-15 18:22:50 +0530

    added login user as well jwt

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,5 +1,6 @@
 import User from "../models/user"
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 export const registerUser=async(req,res)=>{
     try{
@@ -14,5 +15,24 @@ export const registerUser=async(req,res)=>{
         await newUser.save();
     }catch(err){
         console.log("couldn't register",err.message);
+    }
+}
+
+export const loginUser=async()=>{
+    try{
+        const {email,password}=req.body;
+        const existingUser=await User.findOne({email});
+        if(!existingUser){
+            return res.status(400).json({message:"Invalid email or password"});
+        }
+        const match=await bcrypt.compare(password,existingUser.password);
+        if(!match){
+            return res.status(400).json({message:"Invalid email or password"});
+        }
+        const token=jwt.sign({_id:existingUser._id,email:existingUser.email},process.env,JWT_KEY,{expiresIn:"7d"});
+        res.status(200).json({ token, user: { username: user.username, email: user.email } });
+
+    }catch(err){
+        res.status(500).json({ message: "Server error" });
     }
 }
