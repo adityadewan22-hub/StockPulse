bb8e27026348391c7a16a43b0af6173a0447250b
Aditya Dewan <adityadewan22@gmail.com>
2025-10-16 14:06:12 +0530

    added login page

---
+++ frontend/src/app/pages/LoginPage.tsx
@@ -0,0 +1,42 @@
+import axios from "axios";
+import React, { FormEvent, useState } from "react";
+
+const login = () => {
+  //we need state, handle and reutrn;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async (e: React.FormEvent) => {
+    try {
+      e.preventDefault();
+      const res = await axios.post("http://localhost:5000/api/auth/login", {
+        email,
+        password,
+      });
+    } catch (err: any) {
+      console.log("couldnt login", err.message);
+    }
+  };
+
+  return (
+    <div className="flex justify-center items-center h-screen bg-gray-100">
+      <form onSubmit={handleLogin}>
+        <h2>Login</h2>
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        ></input>
+        <input
+          type="password"
+          placeholder="Password"
+          value={email}
+          onChange={(e) => setPassword(e.target.value)}
+        ></input>
+      </form>
+    </div>
+  );
+};
+
+export default login;
