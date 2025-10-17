45ce8bc7138c3e7e017d2b10d429f15ff6cba77d
Aditya Dewan <adityadewan22@gmail.com>
2025-10-18 00:32:05 +0530

    added protected route to index

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,12 +1,17 @@
 import StockCard from "./components/StockCard";
 import StockList from "./components/StockList";
 import StockLiveCard from "./components/StockLiveCard";
+import ProtectRoute from "./routes/protectRoute";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-center min-h-screen bg-gray-950 p-8">
-      <h1 className="text-3xl font-bold text-white mb-6">📊 StockPulse Live</h1>
-      <StockList />
-    </main>
+    <ProtectRoute>
+      <main className="flex flex-col items-center justify-center min-h-screen bg-gray-950 p-8">
+        <h1 className="text-3xl font-bold text-white mb-6">
+          📊 StockPulse Live
+        </h1>
+        <StockList />
+      </main>
+    </ProtectRoute>
   );
 }
