4ae9f123021cbcb994abfcec16aea8b1f114ce8d
Aditya Dewan <adityadewan22@gmail.com>
2025-10-17 19:10:47 +0530

    added middleware for auth

--- backend/src/index.js
+++ backend/src/index.js
@@ -17,7 +17,7 @@ const app = express();
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use("/api/auth",authRouter)
+app.use("/api/auth",authRouter) 
 
 const server = http.createServer(app);
 
