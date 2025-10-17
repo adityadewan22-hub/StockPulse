5e8cd236c2bca1f7436bade9c68c95891e2f7bd0
Aditya Dewan <adityadewan22@gmail.com>
2025-10-17 19:11:52 +0530

    added middleware to stock route

--- backend/src/routes/stockRoutes.js
+++ backend/src/routes/stockRoutes.js
@@ -1,7 +1,9 @@
 import { stockData } from "../controllers/stockController.js";
 import express from "express";
+import verifyToken from "../middleware/authMiddleware.js";
 
 const router= express.Router();
+router.use(verifyToken);
 router.get("/:symbol",stockData);
 
 export default router;
