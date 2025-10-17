e25783bbf154d85498741599c07be2d953f81b5f
Aditya Dewan <adityadewan22@gmail.com>
2025-10-18 00:19:26 +0530

    added context for auth

---
+++ frontend/src/app/context/authContext.tsx
@@ -0,0 +1,35 @@
+import { createContext, useState, useContext } from "react";
+import type { ReactNode } from "react";
+
+interface authContextType {
+  token: string | null;
+  setToken: (token: string | null) => void;
+}
+
+const authContext = createContext<authContextType | undefined>(undefined);
+
+export const AuthProvider = ({ children }: { children: ReactNode }) => {
+  const [token, setToken] = useState<string | null>(
+    localStorage.getItem("token")
+  );
+  const handleSetToken = (newToken: string | null) => {
+    if (newToken) {
+      localStorage.setItem("token", newToken);
+    } else {
+      localStorage.removeItem("token");
+    }
+    setToken(newToken);
+  };
+
+  return (
+    <authContext.Provider value={{ token, setToken: handleSetToken }}>
+      {children}
+    </authContext.Provider>
+  );
+};
+
+export const useAuth = () => {
+  const context = useContext(authContext);
+  if (!context) throw new Error("useAuth must be inside authProvider");
+  return context;
+};
