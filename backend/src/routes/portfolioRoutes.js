2998d4b8c9813a76fe9ef7324b918e389860aefb
Aditya Dewan <adityadewan22@gmail.com>
2025-10-22 02:00:46 +0530

    added portfolio router

---
+++ backend/src/routes/portfolioRoutes.js
@@ -0,0 +1,11 @@
+import verifyToken from "../middleware/authMiddleware";
+import express from express;
+import { buyStock, getPortfolio, sellStock } from "../controllers/portfolioController";
+
+const portfolioRouter=express.router();
+
+portfolioRouter.post("/buy",verifyToken,buyStock);
+portfolioRouter.post("/sell",verifyToken,sellStock);
+portfolioRouter.get("/",verifyToken,getPortfolio);
+
+export default portfolioRouter;
